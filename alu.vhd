cd809bcecfcc83fb6f98b20b500a78095b2f865f
jasnap <popovic.jasna93@gmail.com>
2017-09-07 00:13:28 +0200

    Added algorithm for 16bit op, missing tvalid

--- alu.vhd
+++ alu.vhd
@@ -20,16 +20,15 @@ end entity alu;
 
 architecture Behavioral of alu is
 
-  type data_array is array(natural range <>) of std_logic_vector(7 downto 0);
+ -- type data_array is array(natural range <>) of std_logic_vector(7 downto 0);
   signal a_in, b_in: std_logic_vector(7 downto 0);
-  --signal data_reg: data_array(2 downto 0);
   signal result: std_logic_vector(16 downto 0);
-  signal result_tmp1, result_tmp2 : std_logic_vector(7 downto 0);
+  signal result_low, result_high : std_logic_vector(8 downto 0);
   signal op_code: std_logic_vector (2 downto 0);
   signal addr: integer := 0;
   signal a_plus_b: std_logic_vector(16 downto 0);
   signal a_minus_b: std_logic_vector(16 downto 0);
-  signal cout_sig, cout_sig1, flag : std_logic := '0';
+  signal cout_sig, cout_sig1, flag1, flag2 : std_logic := '0';
 
 begin  -- architecture Behavioral
 
@@ -43,6 +42,7 @@ begin  -- architecture Behavioral
   begin  -- process
     if reset = '0' then                 -- asynchronous reset (active low)
       tready_out <= '1';
+      tvalid_out <= '0';
     elsif clk'event and clk = '1' then  -- rising clock edge
         if tvalid_in = '1' then
           case addr is
@@ -54,7 +54,7 @@ begin  -- architecture Behavioral
                       tready_out <= '0';
                       addr <= addr + 1;
             when 3 => addr <= addr + 1;
-                      tvalid_out <= '1';
+            		  tvalid_out <= '1';
             when 4 => addr <= 0;
                       tready_out <= '1';
                       tvalid_out <= '0';
@@ -72,37 +72,43 @@ begin  -- process
                       cout_sig <= result(8);                  
         when "001" => result <= a_minus_b;                                      --a-b
                       cout_sig <= result(8);
-        when "010" => result <= std_logic_vector(signed(a_in) * signed(b_in));  --a*b
+        when "010" => result(15 downto 0) <= std_logic_vector(unsigned(a_in) * unsigned(b_in));  --a*b
                       cout_sig <= '0';
-        when "011" => result <= std_logic_vector(signed(a_plus_b) srl 1);       --2*(a+b)
+        when "011" => result <= std_logic_vector(unsigned(a_plus_b) sll 1);       --2*(a+b)
                       cout_sig <= result(8);
-        when "100" => result <= std_logic_vector(signed(a_minus_b) srl 1);      --2*(a-b)
+        when "100" => result <= std_logic_vector(unsigned(a_minus_b) sll 1);      --2*(a-b)
                       cout_sig <= result(8);
-        when "101" => result_tmp1 <= a_plus_b(7 downto 0);
-                      cout_sig1 <= a_plus_b(8);
-                      if flag = '1' then
-                        if cout_sig1 = '1' then
-                            result_tmp1 <= std_logic_vector(signed(result_tmp1) + 1);                            
+        when "101" => if flag1 = '1' then
+                        if result_low(8) = '1' then
+                            result(16 downto 8) <= std_logic_vector(unsigned(a_plus_b(8 downto 0)) + 1);
+                        else 
+                        	result(16 downto 8) <= a_plus_b(8 downto 0);                            
                         end if;
-                        result <= cout_sig1 & result_tmp1 & result_tmp2;
-                        cout_sig <= result(16);
-                        flag <= '0';
+                        result(7 downto 0) <= result_low(7 downto 0);
+                        flag1 <= '0';
                       else
-                        result_tmp2 <= result_tmp1;
-                        flag <= '1';
+                        if addr = 2 then
+                        	result_high <= a_plus_b(8 downto 0);
+                        else 
+                        	result_low <= a_plus_b(8 downto 0);
+                        end if;
+                        flag1 <= '1';
                       end if;  
-        when "110" => result_tmp1 <= a_minus_b(7 downto 0);
-                      cout_sig1 <= a_minus_b(8);
-                      if flag = '1' then
-                        if cout_sig1 = '1' then
-                            result_tmp1 <= std_logic_vector(signed(result_tmp1) + 1);                            
+        when "110" => if flag2 = '1' then
+                        if result_low(8) = '1' then
+                            result(16 downto 8) <= std_logic_vector(unsigned(a_minus_b(8 downto 0)) + 1);
+                        else 
+                        	result(16 downto 8) <= a_minus_b(8 downto 0);                            
                         end if;
-                        result <= cout_sig1 & result_tmp1 & result_tmp2;
-                        cout_sig <= result(16);
-                        flag <= '0';
+                        result(7 downto 0) <= result_low(7 downto 0);
+                        flag2 <= '0';
                       else
-                        result_tmp2 <= result_tmp1;
-                        flag <= '1';
+                      	if addr = 2 then
+                        	result_high <= a_minus_b(8 downto 0);
+                        else 
+                        	result_low <= a_minus_b(8 downto 0);
+                        end if;
+                        flag2 <= '1';
                       end if;  
         when others => null;
       end case;   
